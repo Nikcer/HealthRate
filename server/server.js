58b309a551ff6407dd680f1b87f69eee7bd55951
Nikcer <87930631+Nikcer@users.noreply.github.com>
2023-09-30 11:31:38 +0200

    port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const connectDB = require("./database");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 connectDB();
 
 app.listen(PORT, () =>
