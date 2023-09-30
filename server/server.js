72eb7e6c5db71e55097064f29ae9361ac2a59f25
Nikcer <87930631+Nikcer@users.noreply.github.com>
2023-09-30 11:07:18 +0200

    change port

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const connectDB = require("./database");
-
+const PORT = process.env.PORT || 8000;
 connectDB();
 
-app.listen(process.env.PORT, () =>
+app.listen(PORT, () =>
   console.log(`Il server è in ascolto sulla porta ${process.env.PORT}`)
 );
